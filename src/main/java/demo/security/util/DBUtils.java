d8be40e339debead3f76e04675a8cb1ccdfa6f9e
jonathan.vila <jonathan.vila@sonarsource.com>
2023-09-20 11:11:19 +0200

    demo

--- src/main/java/demo/security/util/DBUtils.java
+++ src/main/java/demo/security/util/DBUtils.java
@@ -1,16 +1,37 @@
 package demo.security.util;
 
 import javax.servlet.http.HttpServletRequest;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class DBUtils {
+    Object value;
 
     Connection connection;
+
+
+    /**
+     * The following code shows how to use {@code Optional.isPresent}:
+     * {@snippet :
+             if (v.isPresent()) { // @highlight substring="isPresent"
+               System.out.println("v: " + v.get());
+             }
+     * }
+     * Where v != null
+     */
+    public boolean isPresent() {
+        return value != null;
+    }
+
     public DBUtils() throws SQLException {
         connection = DriverManager.getConnection(
-                "mYJDBCUrl", "myJDBCUser", "myJDBCPass");
+                "mYJDBCUrl", "myJDBCUser", "myJDBCPasswd");
     }
 
     public List<String> findUsers(String user) throws Exception {
