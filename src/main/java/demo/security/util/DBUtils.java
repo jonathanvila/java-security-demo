6edd5a2ad1b6ee400c014824193d425493483876
jonathan.vila <jonathan.vila@sonarsource.com>
2023-10-24 16:48:53 +0200

    demo

--- src/main/java/demo/security/util/DBUtils.java
+++ src/main/java/demo/security/util/DBUtils.java
@@ -10,7 +10,7 @@ public class DBUtils {
     Connection connection;
     public DBUtils() throws SQLException {
         connection = DriverManager.getConnection(
-                "mYJDBCUrl", "myJDBCUser", "codedJDBCPasswd");
+                "mYJDBCUrl", "myJDBCUser", "hardcodedJDBCPasswd");
     }
 
     public List<String> findUsers(String user) throws Exception {
